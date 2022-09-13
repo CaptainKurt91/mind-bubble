0aeffa7cb49a3f98d52d4ea5530246a918743f51
will dunwoody <will.dunwoody@gmail.com>
2022-09-13 08:25:10 +0200

    Fixed navbar add button, changed form to div in task.new view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_12_162706) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_13_053313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_162706) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "end_date"
     t.index ["category_id"], name: "index_category_notes_on_category_id"
   end
 
