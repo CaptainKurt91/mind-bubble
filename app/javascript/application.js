c4128907335b3d5c7bed6cb2c0d8644dc60f0aaa
will dunwoody <will.dunwoody@gmail.com>
2022-09-13 12:50:40 +0200

    fixed changing stated issues

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -30,8 +30,8 @@ document.addEventListener("turbo:load", () => {
 })
 
 
-/* document.addEventListener("turbo:load", () => {
-const listItems = document.querySelectorAll(".boolean");
+document.addEventListener("turbo:load", () => {
+const listItems = document.querySelectorAll(".check-item");
 const checkedItems = []
 
 
@@ -52,5 +52,4 @@ const checkedItems = []
     })
   });
 
-}); */
-// console.log(listItems.checked)
+});
