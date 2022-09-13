a6d86727eae832ede7df5a7b8f6b9198e8c7f714
Leo Llorca <leo.llorca@hotmail.com>
2022-09-13 13:30:51 +0200

    do not care

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import "bootstrap"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import "bootstrap";
 
 document.addEventListener("turbo:load", () => {
   const dropupProfile = document.getElementById("dropup-profile");
@@ -10,30 +10,28 @@ document.addEventListener("turbo:load", () => {
   const dropupRight = document.querySelector(".drp-cnt-right");
   const dropupCenter = document.querySelector(".drp-cnt-cent");
 
-  dropupProfile.addEventListener("click", function(e) {
-      e.preventDefault();
-      if (dropupRight.style.right == "-300px") {
-        dropupRight.style.right = "5px";
-      } else {
-        dropupRight.style.right = "-300px";
-      }
+  dropupProfile.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (dropupRight.style.right == "-300px") {
+      dropupRight.style.right = "5px";
+    } else {
+      dropupRight.style.right = "-300px";
+    }
   });
 
-  dropupAdd.addEventListener("click", function(e) {
-      e.preventDefault();
-      if (dropupCenter.style.display == "none") {
-        dropupCenter.style.display = "block";
-      } else {
-        dropupCenter.style.display = "none";
-      }
+  dropupAdd.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (dropupCenter.style.display == "none") {
+      dropupCenter.style.display = "block";
+    } else {
+      dropupCenter.style.display = "none";
+    }
   });
-})
+});
 
-
-/* document.addEventListener("turbo:load", () => {
-const listItems = document.querySelectorAll(".boolean");
-const checkedItems = []
-
+document.addEventListener("turbo:load", () => {
+  const listItems = document.querySelectorAll(".check-item");
+  const checkedItems = [];
 
   listItems.forEach((item) => {
     item.addEventListener("change", (e) => {
@@ -47,10 +45,8 @@ const checkedItems = []
       const test = listItems.length;
       const test2 = checkedItems.length;
 
-      const progression = document.getElementById("progression")
-      progression.style.width = `${test2 / test * 100}%`
-    })
+      const progression = document.getElementById("progression");
+      progression.style.width = `${(test2 / test) * 100}%`;
+    });
   });
-
-}); */
-// console.log(listItems.checked)
+});
