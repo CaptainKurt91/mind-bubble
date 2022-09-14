90f0d333cf3521e74925b52b6a711a1ff2694910
will dunwoody <will.dunwoody@gmail.com>
2022-09-14 18:19:15 +0200

    Fixed back button....I think

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,12 @@ module ApplicationHelper
   def link_to_previous_page(link_title)
     return if session[:previous_pages].nil?
 
-    url = session[:previous_pages][-2] # or params[:callback] obviously. :)
-    url_w_params = url + (url.include?('?') ? '&' : '?') + 'is_back=true'
+    url = session[:previous_pages][-2]
+    url_w_params = ""
+
+    unless url.nil?
+      url_w_params = url + (url.include?('?') ? '&' : '?') + 'is_back=true'
+    end
 
     link_to_if(
       session[:previous_pages].present? && session[:previous_pages].size > 1,
