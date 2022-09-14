de7b264321a3404a7397cce2c1394aa947a2fbed
will dunwoody <will.dunwoody@gmail.com>
2022-09-14 16:59:05 +0200

    fixed spacing in button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,6 @@ module ApplicationHelper
     url = session[:previous_pages][-2] # or params[:callback] obviously. :)
     url_w_params = url + (url.include?('?') ? '&' : '?') + 'is_back=true'
 
-
     link_to_if(
       session[:previous_pages].present? && session[:previous_pages].size > 1,
       link_title,
