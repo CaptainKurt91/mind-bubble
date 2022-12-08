f5ba98bb246b5f73266426482cb6c49acbbaa07b
will dunwoody <will.dunwoody@gmail.com>
2022-12-08 18:11:10 +0100

    Fixed seeding and relation problem

--- db/migrate/20221206174349_add_user_to_each_table.rb
+++ db/migrate/20221206174349_add_user_to_each_table.rb
@@ -1,6 +1,6 @@
 class AddUserToEachTable < ActiveRecord::Migration[7.0]
   def change
-    add_reference :categories, :user, foreign_key: true
+    # add_reference :categories, :user, foreign_key: true
     add_reference :category_checklist_items, :user, foreign_key: true
     add_reference :category_checklists, :user, foreign_key: true
     add_reference :category_notes, :user, foreign_key: true
