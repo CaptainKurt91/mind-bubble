7408a8b7394d5bd0db35866ea2bb625c778289e3
will dunwoody <will.dunwoody@gmail.com>
2023-01-02 18:00:59 +0100

    removed useless updated checklist method

--- app/javascript/controllers/list_item_controller.js
+++ app/javascript/controllers/list_item_controller.js
@@ -2,18 +2,18 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["state", "progression"];
+  static values = { checklist: Number }
 
   connect() {
     this.updateProgressBar()
   }
 
   toggle(event) {
-    console.log(event.currentTarget.checked)
     let formData = new FormData();
     formData.append(`${event.target.dataset.name}_checklist_item[state]`, event.currentTarget.checked);
     const token = document.getElementsByName("csrf-token")[0].content;
 
-    fetch(`/${event.target.dataset.name}_checklist_items/${event.target.dataset.id}/update_checklist`, {
+    fetch(`/${event.target.dataset.name}_checklists/${this.checklistValue}/${event.target.dataset.name}_checklist_items/${event.target.dataset.id}`, {
       body: formData,
       method: "PATCH",
       credentials: "include",
